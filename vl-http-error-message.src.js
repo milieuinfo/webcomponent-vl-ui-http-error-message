6a950af1aa7bb8cff8bc982c716bf300f9d5686d
Sander Kleykens <sander.kleykens@avnu.be>
2019-12-23 11:25:46 +0100

    Skelet

---
+++ vl-http-error-message.src.js
@@ -0,0 +1,19 @@
+import { VlElement, define } from '/node_modules/vl-ui-core/vl-core.js';
+
+/**
+ * VlHttpErrorMessage
+ * @class
+ * @classdesc 
+ * 
+ * @extends VlElement
+ * 
+ * @property 
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-http-error-message/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-http-error-message/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-http-error-message.html|Demo}
+ * 
+ */
+export class VlHttpErrorMessage extends VlElement(HTMLElement) {}
+
+define('vl-http-error-message', VlHttpErrorMessage);
