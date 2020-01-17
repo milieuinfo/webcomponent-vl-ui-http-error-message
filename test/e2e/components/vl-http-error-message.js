9404247ca801382ffc2a3e7142f9e4b0f3013c18
pascalriquier <pascalriquier@hotmail.com>
2020-01-17 08:59:43 +0100

    Introductie e2e testen (#5)

--- test/e2e/components/vl-http-error-message.js
+++ test/e2e/components/vl-http-error-message.js
@@ -1,20 +1,57 @@
 const { VlElement } = require('vl-ui-core');
 const { VlTypography } = require('vl-ui-typography');
+const { VlButton } = require('vl-ui-button');
 const { By } = require('selenium-webdriver');
 
 class VlHttpErrorMessage extends VlElement {
     async getTitle() {
-        const title = await this.shadowRoot.findElement(By.css('#title'));
-        return title.getText();
+        return (await this._getTitle()).getText();
     }
 
     async getContent() {
-        const typo = new VlTypography(this.driver, this.shadowRoot.findElement(By.css('#text')));
-        return typo.getText();
+        return (await this._getTypography()).getText();
     }
 
-    async getActionButton() {
-        return this.shadowRoot.findElement(By.css('a'));
+    async clickOnAction() {
+        return (await this._getAction()).click();
+    }
+
+    async _getTitle() {
+        return this.shadowRoot.findElement(By.css('#title'));
+    }
+
+    async _getTypography() {
+        const element = await this.shadowRoot.findElement(By.css('#text'));
+        return new VlTypography(this.driver, element);
+    }
+
+    async _getAction() {
+        const element = await this.shadowRoot.findElement(By.css('a'));
+        return new VlButton(this.driver, element);
+    }
+
+    async _getImage() {
+        const smallImage = await this._getSmallImage();
+        const normalImage = await this._getNormalImage();
+        const smallImageSrc = await smallImage.getAttribute('src');
+        const normalImageSrc = await normalImage.getAttribute('src');
+        const smallImageAlt = await smallImage.getAttribute('alt');
+        const normalImageAlt = await normalImage.getAttribute('alt');
+        if (smallImageSrc != normalImageSrc) {
+        	throw new Error("Small en normal image src moeten gelijk zijn");
+        }
+        if (smallImageAlt != normalImageAlt) {
+        	throw new Error("Small en normal image alt moeten gelijk zijn");
+        }
+        return smallImage;
+    }
+
+    async _getNormalImage() {
+        return this.shadowRoot.findElement(By.css('#image-normal'));
+    }
+
+    async _getSmallImage() {
+        return this.shadowRoot.findElement(By.css('#image-small'));
     }
 }
 
--- test/e2e/http-error-message.test.js
+++ test/e2e/http-error-message.test.js
@@ -1,33 +1,64 @@
 const { assert, driver } = require('vl-ui-core').Test;
-const VlHttpErrorMessage = require('./pages/vl-http-error-message.page');
+const VlHttpErrorMessagePage = require('./pages/vl-http-error-message.page');
 
 describe('vl-http-error-message', async () => {
-    const vlHttpErrorMessage = new VlHttpErrorMessage(driver);
+    const vlHttpErrorMessagePage = new VlHttpErrorMessagePage(driver);
 
     before(() => {
-        return vlHttpErrorMessage.load();
+        return vlHttpErrorMessagePage.load();
     });
 
-    it('klikken op de actieknop van een 404 bericht zal de gebruiker omleiden naar de startpagina', async () => {
-        const originalUrl = await driver.getCurrentUrl();
-        assert.isFalse(originalUrl.endsWith('/'));
-
-        await vlHttpErrorMessage.clickOnButton();
-
-        const urlAfterClick = await driver.getCurrentUrl();
-        assert.isTrue(urlAfterClick.endsWith('/'));
-
-        await vlHttpErrorMessage.load();
-    });
-
-    it('titel van de error message word getoond bij een standaard component', async () => {
-        const message = await vlHttpErrorMessage.getStandaardErrorMessageComponent();
+    it('als gebruiker kan ik de foutmelding titel lezen', async () => {
+        let message = await vlHttpErrorMessagePage.getNotFoundErrorMessage();
         await assert.eventually.equal(message.getTitle(), 'Oeps, die pagina vonden we niet terug');
+
+        message = await vlHttpErrorMessagePage.getCustomErrorMessage();
+        await assert.eventually.equal(message.getTitle(), 'Niets gevonden hiervoor.');
     });
-    
-    it('content van de error message word getoond bij een standaard component', async () => {
-        const message = await vlHttpErrorMessage.getStandaardErrorMessageComponent();
+
+    it('als gebruiker kan ik de foutmelding content lezen', async () => {
+        let message = await vlHttpErrorMessagePage.getNotFoundErrorMessage();
         await assert.eventually.equal(message.getContent(), 'De pagina die u zoekt, vonden we niet terug.');
 
-    })
+        message = await vlHttpErrorMessagePage.getCustomErrorMessage();
+        await assert.eventually.equal(message.getContent(), 'Sorry, er liep iets onverwachts mis.');
+    });
+
+    it('als gebruiker kan ik de foutmelding link zien', async () => {
+        let httpErrorMessage = await vlHttpErrorMessagePage.getNotFoundErrorMessage();
+        let link = await httpErrorMessage._getAction();
+        await assert.eventually.equal(link.getText(), 'Terug naar de startpagina');
+
+        httpErrorMessage = await vlHttpErrorMessagePage.getCustomErrorMessage();
+        link = await httpErrorMessage._getAction();
+        await assert.eventually.equal(link.getText(), 'Opnieuw opstarten');
+    });
+
+    it('als gebruiker kan ik de foutmelding afbeelding zien', async () => {
+        let httpErrorMessage = await vlHttpErrorMessagePage.getNotFoundErrorMessage();
+        let image = await httpErrorMessage._getImage();
+        await assert.eventually.equal(image.getAttribute('src'), 'https://cdn.milieuinfo.be/http-error-message-assets/LATEST/img/page-not-found.svg');
+        await assert.eventually.equal(image.getAttribute('alt'), 'Pagina niet gevonden');
+
+        httpErrorMessage = await vlHttpErrorMessagePage.getCustomErrorMessage();
+        image = await httpErrorMessage._getImage();
+        await assert.eventually.equal(image.getAttribute('src'), 'http://localhost:8080/demo/error-404.png');
+        await assert.eventually.equal(image.getAttribute('alt'), 'Niets gevonden');
+    });
+
+    it('als gebruiker kan ik op de actieknop van een 404 foutmelding klikken om naar de startpagina te gaan', async () => {
+        let originalUrl = await driver.getCurrentUrl();
+        assert.isFalse(originalUrl.endsWith('/'));
+        let httpErrorMessage = await vlHttpErrorMessagePage.getNotFoundErrorMessage();
+        await httpErrorMessage.clickOnAction();
+        let urlAfterClick = await driver.getCurrentUrl();
+        assert.isTrue(urlAfterClick.endsWith('/'));
+
+        await vlHttpErrorMessagePage.load();
+        httpErrorMessage = await vlHttpErrorMessagePage.getCustomErrorMessage();
+        await httpErrorMessage.clickOnAction();
+        urlAfterClick = await driver.getCurrentUrl();
+        assert.isTrue(urlAfterClick.endsWith('#demo'));
+        await vlHttpErrorMessagePage.load();
+    });
 });
