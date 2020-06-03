aa6b8726ed2890783dfa5aeb2aab9009d909ceb3
coemans <tom.coemans@gmail.com>
2020-06-03 13:30:44 +0200

    Introductie vl-ui-demo en ESlint (#26)

--- test/e2e/pages/vl-http-error-message.page.js
+++ test/e2e/pages/vl-http-error-message.page.js
@@ -1,26 +1,26 @@
 const VlHttpErrorMessage = require('../components/vl-http-error-message');
-const { Page, Config } = require('vl-ui-core').Test;
+const {Page, Config} = require('vl-ui-core').Test;
 
 class VlHttpErrorMessagePage extends Page {
-    async get404() {
-        return this._getHttpErrorMessage('#error-404');
-    }
+  async get404() {
+    return this._getHttpErrorMessage('#error-404');
+  }
 
-    async get500() {
-        return this._getHttpErrorMessage('#error-500');
-    }
+  async get500() {
+    return this._getHttpErrorMessage('#error-500');
+  }
 
-    async getCustomErrorMessage() {
-    	return this._getHttpErrorMessage('#custom-message');
-    }
-    
-    async load() {
-        await super.load(Config.baseUrl + '/demo/vl-http-error-message.html');
-    }
+  async getCustomErrorMessage() {
+    return this._getHttpErrorMessage('#custom-message');
+  }
 
-    async _getHttpErrorMessage(selector) {
-        return new VlHttpErrorMessage(this.driver, selector);
-    }
+  async load() {
+    await super.load(Config.baseUrl + '/demo/vl-http-error-message.html');
+  }
+
+  async _getHttpErrorMessage(selector) {
+    return new VlHttpErrorMessage(this.driver, selector);
+  }
 }
 
 module.exports = VlHttpErrorMessagePage;
