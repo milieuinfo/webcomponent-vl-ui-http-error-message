d9d0a22228f0da474fd216ab2b7b5b19da515c4c
coemans <tom.coemans@vlaanderen.be>
2021-02-03 17:32:14 +0100

    update dependencies (#50)

--- dist/vl-http-505-message.src.js
+++ dist/vl-http-505-message.src.js
@@ -1,5 +1,5 @@
 import {define} from 'vl-ui-core';
-import {VlHttpErrorMessage} from 'vl-ui-http-error-message/dist/vl-http-error-message.src.js';
+import {VlHttpErrorMessage} from '../dist/vl-http-error-message.src.js';
 
 define('vl-http-505-message', class extends VlHttpErrorMessage {
   constructor() {
